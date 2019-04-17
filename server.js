a572ec59ac0ebd601349fac09c2709a5a77559cf
Karl <karl.steltenpohl@gmail.com>
2019-04-17 13:47:55 -0500

    server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const bodyParser = require("body-parser");
 
 
 const app = express();
-app.use(bodyParser.json());
+//app.use(bodyParser.json());
 
 // Create link to Angular build directory
 var distDir = __dirname + "/dist/";
