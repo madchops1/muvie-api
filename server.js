ad52df655ab23f8dc5e6fd07c90346ad6eb9f297
Karl <karl.steltenpohl@gmail.com>
2019-04-17 13:35:40 -0500

    server.js

--- server.js
+++ server.js
@@ -1,9 +1,29 @@
 //Install express server
 const express = require('express');
 const path = require('path');
+const bodyParser = require("body-parser");
+
 
 const app = express();
+app.use(bodyParser.json());
 
+// Create link to Angular build directory
+var distDir = __dirname + "/dist/";
+app.use(express.static(distDir));
+
+// Initialize the app.
+var server = app.listen(process.env.PORT || 8080, function () {
+    var port = server.address().port;
+    console.log("App now running on port", port);
+});
+
+// Generic error handler used by all endpoints.
+function handleError(res, reason, message, code) {
+    console.log("ERROR: " + reason);
+    res.status(code || 500).json({"error": message});
+}
+
+/*
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/muvie'));
 
@@ -13,3 +33,8 @@ app.get('/*', function(req,res) {
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
+*/
+
+app.get("/api/make", function(req, res) {
+    res.status(200).json();
+});
