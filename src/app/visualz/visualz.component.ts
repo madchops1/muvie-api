4bd96c82095024229ee2e9d46876535f916525ce
Karl Steltenpohl <madchops@Karls-MBP.local>
2019-10-16 00:54:12 -0500

    added community disqus to home page

--- src/app/visualz/visualz.component.ts
+++ src/app/visualz/visualz.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { DeviceDetectorService } from 'ngx-device-detector';
+import { BrowserModule, DomSanitizer } from '@angular/platform-browser'
 
 @Component({
   selector: 'app-visualz',
@@ -9,14 +10,40 @@ import { DeviceDetectorService } from 'ngx-device-detector';
 export class VisualzComponent implements OnInit {
 
     deviceInfo: any;
+    htmlScript: any = '';
 
-    constructor(private deviceService: DeviceDetectorService) { 
+    constructor(private deviceService: DeviceDetectorService, private sanitizer: DomSanitizer) { 
 
     }
 
     ngOnInit() {
         this.deviceInfo = this.deviceService.getDeviceInfo();
         console.log('deviceInfo', this.deviceInfo);
+
+        this.htmlScript = this.sanitizer.bypassSecurityTrustHtml(`
+      <script>
+
+    /**
+    *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+    *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables*/
+    /*
+    var disqus_config = function () {
+    this.page.url = PAGE_URL;  // Replace PAGE_URL with your page's canonical URL variable
+    this.page.identifier = PAGE_IDENTIFIER; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+    };
+    */
+    (function () { // DON'T EDIT BELOW THIS LINE
+        var d = document, s = d.createElement('script');
+        s.src = 'https://visualz.disqus.com/embed.js';
+        s.setAttribute('data-timestamp', +new Date());
+        (d.head || d.body).appendChild(s);
+    })();
+</script>
+    < noscript > Please enable JavaScript to view the < a href = "https://disqus.com/?ref_noscript" > comments powered by
+Disqus.< /a></noscript >`);
+
+        const fragment = document.createRange().createContextualFragment(this.htmlScript);
+        document.body.appendChild(fragment);
     }
 
 
