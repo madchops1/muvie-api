4d913dad271c04560e0a328a79fa7bcb87079a2a
Karl Steltenpohl <madchops@Karls-MBP.local>
2020-04-03 15:46:39 -0500

    ...

--- src/app/live-stream/live-stream.component.ts
+++ src/app/live-stream/live-stream.component.ts
@@ -2809,13 +2809,12 @@ export class LiveStreamComponent implements OnInit {
     imageEffectsThree(): any {
         //if (audioReactive) {
         // Shape Volume Scale
-        if (this.bump) {
-            let gotoScale = (this.volume * 1.2 + .1) * 2;
-            this.scl += (gotoScale - this.scl) / 3;
-            //console.log(this.scl);
-            if (this.scl <= 1.0) { this.scl = 1.0; }
-            this.__cube.scale.x = this.__cube.scale.y = this.__cube.scale.z = this.scl;
-        }
+        let gotoScale = (this.volume * 1.2 + .1) * 2;
+        this.scl += (gotoScale - this.scl) / 3;
+        //console.log(this.scl);
+        if (this.scl <= 1.0) { this.scl = 1.0; }
+        this.__cube.scale.x = this.__cube.scale.y = this.__cube.scale.z = this.scl;
+
         // let gotoScale = (this.volume * 1.2 + .1) * 2;
         // this.scl += (gotoScale - this.scl) / 3;
         // this.videoSize = this.screenH / 2 * this.scl;
@@ -2863,10 +2862,6 @@ export class LiveStreamComponent implements OnInit {
         if (this.searchGifs && !this.gifResults.length) {
             this.search();
         }
-    }
-
-    toggleBump() {
-        this.bump = !this.bump;
     }
 
     // Close all the overlays
