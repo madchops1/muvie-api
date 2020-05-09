c3ab25e65e3ac48c71ec3f44687058c233eee4f8
Karl Steltenpohl <madchops@Karls-MBP.local>
2020-05-09 13:36:54 -0500

    ...

--- src/app/download/download.component.ts
+++ src/app/download/download.component.ts
@@ -15,7 +15,7 @@ export class DownloadComponent implements OnInit {
     stripeScriptTest: any = '';
     testMode: Boolean = false;
     yearly: Boolean = false;
-    version: '2.1.0';
+    version: '2.1.1';
 
     constructor(private sanitizer: DomSanitizer, private mixpanelService: MixpanelService) { }
 
