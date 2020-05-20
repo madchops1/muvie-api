13f5750d0c8914d6c4b25f0901b107ce17a0a7e7
Karl Steltenpohl <madchops@Karls-MBP.local>
2020-05-20 18:46:06 -0500

    2.1.3

--- src/app/download/download.component.ts
+++ src/app/download/download.component.ts
@@ -15,7 +15,7 @@ export class DownloadComponent implements OnInit {
     stripeScriptTest: any = '';
     testMode: Boolean = false;
     yearly: Boolean = false;
-    version: '2.1.2';
+    version: '2.1.3';
 
     constructor(private sanitizer: DomSanitizer, private mixpanelService: MixpanelService) { }
 
