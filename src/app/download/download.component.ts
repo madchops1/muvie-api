b6131c37871c65955d7c9b04c1a3aef1faca92c6
Karl Steltenpohl <karl.steltenpohl@gmail.com>
2024-10-01 14:30:45 -0600

    v2.2.11

--- src/app/download/download.component.ts
+++ src/app/download/download.component.ts
@@ -15,7 +15,7 @@ export class DownloadComponent implements OnInit {
     stripeScriptTest: any = '';
     testMode: Boolean = false;
     yearly: Boolean = false;
-    version: '2.2.10';
+    version: '2.2.11';
 
     constructor(private sanitizer: DomSanitizer, private mixpanelService: MixpanelService) { }
 
